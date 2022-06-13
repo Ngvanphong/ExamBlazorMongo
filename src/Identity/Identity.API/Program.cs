50c5b4817f27bb11279d9899407197122a314d83
Ngvanphong <ngvanphong2012@gmail.com>
2022-06-13 11:51:42 +0700

    62533

--- src/Identity/Identity.API/Program.cs
+++ src/Identity/Identity.API/Program.cs
@@ -1,4 +1,16 @@
+using System.Collections.Immutable;
+using System;
+using System.Reflection;
+using Identity.API.Database;
+using Identity.API.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using IdentityServer4.Services;
+using Identity.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+var migrationAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 
@@ -6,6 +18,63 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    option => option.UseSqlServer(connectionString,
+    sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly(migrationAssembly);
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null
+        );
+    }
+    )
+);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(x =>
+{
+    x.IssuerUri = "https:/tedu.com.vn";
+    x.Authentication.CookieLifetime = TimeSpan.FromHours(2);
+})
+.AddDeveloperSigningCredential()
+.AddAspNetIdentity<ApplicationUser>()
+.AddConfigurationStore(options =>
+{
+    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+    sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly(migrationAssembly);
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null
+        );
+    }
+    );
+})
+.AddOperationalStore(options =>
+{
+    options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+    sqlServerOptionsAction: sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly(migrationAssembly);
+        sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null
+        );
+    }
+    );
+})
+.Services.AddTransient<IProfileService,ProfileService>();
+
 
 var app = builder.Build();
 
@@ -18,7 +87,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseIdentityServer();
+
 app.UseAuthorization();
+
 
 app.MapControllers();
 
